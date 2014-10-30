683b5639aa25c8ab854969a2caa024df1979bfd8
Maximilian Fischer <mafi@inagile.org>
2014-10-30 15:21:32 +0000

    Add own play_stdin(supports multiple hosts as parameters)

--- examples/play_stdin.js
+++ examples/play_stdin.js
@@ -1,53 +1,61 @@
 var airtunes = require('../lib/'),
+    child_process = require('child_process'),
     argv = require('optimist')
       .usage('Usage: $0 --host [host] --port [num] --volume [num] --password [string]')
       .default('host', 'localhost')
       .default('port', 5000)
       .default('volume', 50)
       .demand(['host'])
-      .argv;
+      .argv,
+      config = require('../lib/config.js');
 
 console.log('pipe PCM data to play over AirTunes');
 console.log('example: cat sample.pcm | node play_stdin.js --host <AirTunes host>\n');
 
-// Only works on OSX
-// airtunes.addCoreAudio();
-
-console.log('adding device: ' + argv.host + ':' + argv.port);
-var device = airtunes.add(argv.host, argv);
-
-
-// when the device is online, spawn ffmpeg to transcode the file
-device.on('status', function(status) {
-  process.stdin.on('data', function () {});
-  process.stdin.pipe(airtunes);
-  process.stdin.resume();
-});
-
-device.on('error', function(err) {
-  console.log('device error: ' + err);
-  process.exit(1);
-});
-
-setTimeout(function () {
-  console.log('stopping');
-  airtunes.stopAll(function () {
-    console.log('all stopped');
+var shairport =
+  child_process.exec('/home/mafi/shairport/shairport -o pipe ' +
+    '/home/mafi/node_airtunes/examples/rawpcm.pcm',
+    function (error, stdout, stderr) {
+      if (error) {
+        console.log(error.stack);
+        console.log('Error code: '+error.code);
+        console.log('Signal received: '+error.signal);
+      }
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
   });
-}, 1000);
+
+shairport.on('exit', function (code) {
+  console.log('Child process exited with exit code '+code);
+});
+
+
+var hosts = argv.host.split(' ');
+var devices = {};
+
+// process event handlers
+process.stdin.on('data', function () {
+});
+
+process.stdin.on('error', function () {
+});
+
+process.stdin.pipe(airtunes);
+process.stdin.resume();
+
+hosts.forEach(function(host) {
+  devices[host] = airtunes.add(host, argv);
+  console.log('adding device: ' + host + ':' + argv.port);
+});
+
+airtunes.on('drain', function(e) {});
 
 // monitor buffer events
 airtunes.on('buffer', function(status) {
   console.log('buffer ' + status);
-
-  // after the playback ends, give AirTunes some time to finish
-  if(status === 'end') {
-    console.log('playback ended, waiting for AirTunes devices');
-    setTimeout(function() {
-      airtunes.stopAll(function() {
-        console.log('end');
-        process.exit();
-      });
-    }, 2000);
-  }
+  if(status === 'end') {}
 });
+
+process.on('exit', function(code) {
+  shairport.kill();
+})
